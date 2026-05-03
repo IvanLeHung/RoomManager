600cecd8e92f1c61891d22b33c557cf9c12620f6
IvanLeHung <lehunghanhchinh@gmail.com>
2026-05-04 02:01:25 +0700

    Trim DATABASE_URL in prisma.js

--- api/lib/prisma.js
+++ api/lib/prisma.js
@@ -6,7 +6,7 @@ const ws = require('ws');
 // Cấu hình WebSocket cho môi trường Serverless
 neonConfig.webSocketConstructor = ws;
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL.trim();
 
 if (!connectionString) {
   throw new Error("DATABASE_URL is not defined in environment variables");
