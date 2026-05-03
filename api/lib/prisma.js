c5f00817e918bd4410eb329df6cc51b53fc9ce9f
IvanLeHung <lehunghanhchinh@gmail.com>
2026-05-04 01:41:57 +0700

    Add files via upload

--- api/lib/prisma.js
+++ api/lib/prisma.js
@@ -1,14 +1,12 @@
 const { PrismaClient } = require('@prisma/client');
 
-let prisma;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+// Khởi tạo Prisma với URL lấy từ biến môi trường
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 module.exports = prisma;
